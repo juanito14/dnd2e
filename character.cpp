30a668c79aaca44839a7b9d18c806ec57dbaed8c
konovalov.s <skonovalov@oao-trk.ru>
2015-07-07 19:22:25 +0300

    added StrengthAbilities

--- character.cpp
+++ character.cpp
@@ -1,18 +1,19 @@
 #include "character.h"
 
 Character::Character(QObject *parent) :
-    QObject(parent),
-    _defaultAbilityValue(8),
-    _availabelAbilityValues(10),
-    _expirience(0)
+	QObject(parent),
+	_defaultAbilityValue(8),
+	_availabelAbilityValues(10),
+	_expirience(0),
+	_level(0)
 {
-    _AbilitiesVector.reserve(MaxAbility);
-    _AbilitiesVector[Strength] = _defaultAbilityValue;
-    _AbilitiesVector[Dexterity] = _defaultAbilityValue;
-    _AbilitiesVector[Constitution] = _defaultAbilityValue;
-    _AbilitiesVector[Intelligence] = _defaultAbilityValue;
-    _AbilitiesVector[Wisdom] = _defaultAbilityValue;
-    _AbilitiesVector[Charisma] = _defaultAbilityValue;
+	_AbilitiesVector.reserve(MaxAbility);
+	_AbilitiesVector[Strength] = _defaultAbilityValue;
+	_AbilitiesVector[Dexterity] = _defaultAbilityValue;
+	_AbilitiesVector[Constitution] = _defaultAbilityValue;
+	_AbilitiesVector[Intelligence] = _defaultAbilityValue;
+	_AbilitiesVector[Wisdom] = _defaultAbilityValue;
+	_AbilitiesVector[Charisma] = _defaultAbilityValue;
 }
 
 Character::~Character()
@@ -22,32 +23,67 @@ Character::~Character()
 
 void Character::checkAbilityType(const Abilities abilityType) const
 {
-    if(abilityType < 0 || abilityType >= MaxAbility){
-        throw std::runtime_error("unknown abilityType");
-    }
-    return;
+	if(abilityType < 0 || abilityType >= MaxAbility){
+		throw std::runtime_error("unknown abilityType");
+	}
+	return;
+}
+
+void Character::checkStrengthAbilityType(const StrengthAbilities strengthAbilityType) const
+{
+	if(strengthAbilityType < 0 || strengthAbilityType >= MaxStrengthAbility){
+		throw std::runtime_error("unknown strengthAbilityType");
+	}
+	return;
 }
 int Character::expirience() const
 {
-    return _expirience;
+	return _expirience;
 }
 
 void Character::increaseExpirience(int expirience)
 {
-    _expirience += expirience;
+	if(_class.getClass() == Fighter && ability(Strength) >= 16){
+		_expirience += expirience + expirience/10;
+		return;
+	}
+
+	_expirience += expirience;
+	return;
+}
+
+int Character::getStrengthAbility(const StrengthAbilities strengthAbilityType) const
+{
+	checkStrengthAbilityType(strengthAbilityType);
+	if(ability(Strength) >= 18){
+		//TODO: exceptional strength бросок процентного кубика
+	}
+	switch (strengthAbilityType) {
+	case HitPossibility:
+
+		break;
+	case DamageAdjust:
+		break;
+	case WeightAllow:
+		break;
+	case OpenDoors:
+		break;
+	default:
+		break;
+	}
 }
 
 
 int Character::ability(const Abilities abilityType) const
 {
-    checkAbilityType(abilityType);
-    return _AbilitiesVector.at(abilityType);
+	checkAbilityType(abilityType);
+	return _AbilitiesVector.at(abilityType);
 }
 
 void Character::increaseAbility(const int increment, const Abilities abilityType)
 {
-    checkAbilityType(abilityType);
-    _AbilitiesVector[abilityType] += increment;
+	checkAbilityType(abilityType);
+	_AbilitiesVector[abilityType] += increment;
 }
 
 
--- character.h
+++ character.h
@@ -7,31 +7,36 @@
 #include "characterclass.h"
 
 enum Abilities {Strength=0, Dexterity, Constitution, Intelligence, Wisdom, Charisma, MaxAbility};
+enum StrengthAbilities {HitPossibility = 0, DamageAdjust, WeightAllow, OpenDoors, MaxStrengthAbility};
 
 class Character : public QObject
 {
-    Q_OBJECT
+	Q_OBJECT
 public:
-    explicit Character(QObject *parent = 0);
-    ~Character();
+	explicit Character(QObject *parent = 0);
+	~Character();
 
-    int ability(const Abilities abilityType) const;
-    void increaseAbility(int increment, const Abilities abilityType);
+	int ability(const Abilities abilityType) const;
+	void increaseAbility(int increment, const Abilities abilityType);
 
-    int expirience() const;
-    void increaseExpirience(int expirience);
+	int expirience() const;
+	void increaseExpirience(int expirience);
+
+	int getStrengthAbility(const StrengthAbilities strengthAbilityType) const;
 
 signals:
 
 public slots:
 
 private:
-    QVector<int> _AbilitiesVector;
-    const int _defaultAbilityValue;
-    int _availabelAbilityValues;
-    void checkAbilityType(const Abilities abilityType) const;
-    CharacterClass _class;
-    int _expirience;
+	QVector<int> _AbilitiesVector;
+	const int _defaultAbilityValue;
+	int _availabelAbilityValues;
+	void checkAbilityType(const Abilities abilityType) const;
+	void checkStrengthAbilityType(const StrengthAbilities strengthAbilityType) const;
+	CharacterClass _class;
+	int _expirience;
+	int _level;
 };
 
 #endif // CHARACTER_H
